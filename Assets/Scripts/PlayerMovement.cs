f64c8da3fa4f5b802ece7e51f1b9f4b96c62d61b
Jah <kennedye2@vcu.edu>
2020-05-19 20:06:01 -0400

    added gravity in function

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed1 = 12f;
     public float speed2 = 2f;
+
+    public float gravity;
 
     // Controls are determined based on the active view, which is managed by CameraManager.cs
     public Camera firstPerson;
@@ -14,35 +16,66 @@ public class PlayerMovement : MonoBehaviour
 
     void Update() {
 
+        Gravity();
+
         if (firstPerson.enabled){
-            FirstPersonControls();
+            FirstPersonControls(speed1);
         } else if (thirdPerson.enabled){
-            ThirdPersonControls();
+            ThirdPersonControls(speed2);
         }
     }
 
-    void FirstPersonControls(){
+    void FirstPersonControls(float pace){
         //Mouse controls 'look', character moves with QRTY relative to direction of current 'look';
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * pace * Time.deltaTime);
     }
 
-    void ThirdPersonControls(){
+    void ThirdPersonControls(float pace){
         transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
 
         if (Input.GetKey("w")){
-            controller.Move(transform.forward * speed2 * Time.deltaTime);
+            ControllerMove("forward", pace);
         }
+        
         if (Input.GetKey("s")){
-            controller.Move(transform.forward * speed2 * Time.deltaTime * -1f);
+            ControllerMove("back", pace);
         }
+
         if (Input.GetKey("d")){
-            controller.Move(transform.right * speed2 * Time.deltaTime);
+            ControllerMove("right", pace);
         }
+
         if (Input.GetKey("a")){
-            controller.Move(transform.right * speed2 * Time.deltaTime * -1f);
+            ControllerMove("left", pace);
+        }
+
+    }
+
+    void ControllerMove(string direction, float pace){
+
+        switch(direction){
+            case "forward": controller.Move(transform.forward * pace * Time.deltaTime);
+            break;
+
+            case "back": controller.Move(transform.forward * pace * Time.deltaTime * -1f);
+            break;
+
+            case "right": controller.Move(transform.right * pace * Time.deltaTime);
+            break;
+
+            case "left": controller.Move(transform.right * pace * Time.deltaTime * -1f);
+            break;
+        }
+    }
+
+    void Gravity(){
+        gravity -= 9.81f * Time.deltaTime;
+        controller.Move(new Vector3(0, gravity, 0));
+        if (controller.isGrounded){
+            gravity = 0;
         }
     }
 
