4bfa06d81970532a5bb2346535f5c789bd9e0e76
Jah <kennedye2@vcu.edu>
2020-05-26 23:25:08 -0400

    working health bar that decrements over time :)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int maxHealth = 8;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public int metabolism = 30;
+
+    void Start() {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        InvokeRepeating("Hunger", metabolism, metabolism);
+    }
+
+    void Update() {
+        //Eat(2);
+    }
+
+    void Eat(int calories){
+        currentHealth += calories;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void Hunger(){
+        currentHealth -= 1;
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
