67163342792b148a7e5bf546601f3941bb9c5986
Jah <kennedye2@vcu.edu>
2021-08-05 20:42:17 +0200

    added public vertical limits to MouseLook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -39,7 +39,7 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, lookLimitDown, lookLimitUp);
+        xRotation = Mathf.Clamp(xRotation, lookLimitUp, lookLimitDown);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
