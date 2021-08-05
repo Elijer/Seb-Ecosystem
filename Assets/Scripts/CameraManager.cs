2c35e97b811229e89734f2f39571fb072b26381a
Jah <kennedye2@vcu.edu>
2021-08-05 20:13:23 +0200

    refactored CameraManager to loop through cameras array

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,43 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// * This color comment is about refacturing
-
-/* What I should really do is to create an array that is full of the cameras and
-create a function that first selects one and makes it active as well as it's audio listener
-and then immediately goes through all others and makes them inactive.
-In addition, the hotkeys for the cameras should be command+1, command+2, etc,
-that way I won't select them accidentally when I'm putting in the public values.
-Is it possible to automatically create an array of cameras in the scene instead of selecting them 
-one by one? Dragging them all in one by one would honestly be great too, but I think it's worth looking
-into. */
-
-/* Yeah this page in the docs lists a method called 'allCameras' which lists all enabled cameras in
-the scene. Which isn't exactly what I want, but this page is a start.*/
+// Notes
+// the .Length property doesn't return the last index, it returns the number of items in the array.
 
 public class CameraManager : MonoBehaviour {
 
-    // * initialize together
-    public Camera[] cameras = new Camera[4];
-    public Camera cam1, cam2, cam3, cam4;
+    // camera array
+    public Camera[] cameras = new Camera[3];
     public Transform player;
 
     void Start(){
-        // Which camera does the player start on
-        CamSwitcher(1);    //Switches the camera
+        // Debug.Log(cameras.Length); > 4
+        CamSwitcher(0); //sets starting camera
     }
 
     void Update() {
-        KeyScanner();      //Switches camera according to Key commands
+        KeyScanner(); //listens to key commands and switches camera appropriately
     }
 
     void KeyScanner(){
-        int cameraSelection;
-        System.Int32.TryParse(Input.inputString, out cameraSelection);
-        CamSwitcher(cameraSelection);
+        string keyInput = Input.inputString;
+        if (keyInput != ""){
+            int validSelection;
+            System.Int32.TryParse(Input.inputString, out validSelection);
+            if (validSelection <= cameras.Length + 1 && validSelection > 0){
+                CamSwitcher(validSelection - 1);
+            }
+        }
     }
 
-    // * Not yet used
+    
     void configureCamera(Camera cam, bool isActive){
         cam.enabled = isActive;
         cam.GetComponent<AudioListener>().enabled = isActive;
@@ -46,35 +39,72 @@ public class CameraManager : MonoBehaviour {
 
 
     void CamSwitcher(int cam){
+        for(int i = 0; i < cameras.Length; i++ ){
+            if (i == cam){
+                configureCamera(cameras[i], true);
+            } else {
+                configureCamera(cameras[i], false);
+            }
+        }
+
+        switch (cam) {  
+            case 0:
+                Cursor.lockState = CursorLockMode.Locked;
+                break;
+            case 1:
+                RotationReset();
+                Cursor.lockState = CursorLockMode.None;
+                break;
+            case 2:
+                RotationReset();
+                Cursor.lockState = CursorLockMode.Locked;
+                break;
+            default:
+                Cursor.lockState = CursorLockMode.Locked;
+                RotationReset();
+                break;
+        }
+
+        
+
+
+
+/*         configureCamera(cameras[0], true);
+        configureCamera(cameras[1], false);
+        configureCamera(cameras[2], false);
+        configureCamera(cameras[3], false); */
+    
+
+        // iterate through cameras array. Activate the match, deactivate everything else.
+/*         for(int i = 0; i < cameras.Length; i++ ){
+            configureCamera(cameras[i], true);
+            if (i != cam - 1){
+                configureCamera(cameras[i], false);
+            }
+        }
 
         if (cam == 1){
+
             Cursor.lockState = CursorLockMode.Locked;
-			cam1.enabled = true;    cam1.GetComponent<AudioListener>().enabled = true;
-			cam2.enabled = false;   cam2.GetComponent<AudioListener>().enabled = false;
-            cam3.enabled = false;   cam3.GetComponent<AudioListener>().enabled = false;
-            cam4.enabled = false;   cam4.GetComponent<AudioListener>().enabled = false;
-        } else if (cam == 2){
+
+        } else if (cam == 2)
+        {
             RotationReset();
+
             Cursor.lockState = CursorLockMode.None;
-			cam1.enabled = false;   cam1.GetComponent<AudioListener>().enabled = false;
-			cam2.enabled = true;    cam2.GetComponent<AudioListener>().enabled = true;
-            cam3.enabled = false;   cam3.GetComponent<AudioListener>().enabled = false;
-            cam4.enabled = false;   cam4.GetComponent<AudioListener>().enabled = false;
+
         } else if (cam == 3){
+
             RotationReset();
             Cursor.lockState = CursorLockMode.Locked;
-			cam1.enabled = false;   cam1.GetComponent<AudioListener>().enabled = false;
-			cam2.enabled = false;   cam2.GetComponent<AudioListener>().enabled = false;
-            cam3.enabled = true;    cam3.GetComponent<AudioListener>().enabled = true;
-            cam4.enabled = false;   cam4.GetComponent<AudioListener>().enabled = false;
+
         } else if (cam == 4){
-			cam1.enabled = false;   cam1.GetComponent<AudioListener>().enabled = false;
-			cam2.enabled = false;   cam2.GetComponent<AudioListener>().enabled = false;
-            cam3.enabled = false;   cam3.GetComponent<AudioListener>().enabled = false;
-            cam4.enabled = true;    cam4.GetComponent<AudioListener>().enabled = true;
+
         } else {
+
             Debug.Log("CamSwitcher doesn't take that as an argument, or that camera doesn't exist");
-        }
+
+        } */
     }
 
     void RotationReset(){
